25e727c1165736aa2030cf8a9a97d5917059dc8b
mastrocolac1atwit <mastrocolac1@wit.edu>
2023-04-10 21:07:22 -0400

    Added Powerups as an abstract class.

--- Powerups.java
+++ Powerups.java
@@ -1,4 +1,32 @@
 
 public abstract class Powerups {
 
+	private int quantity;
+	private String name;
+	private int price;
+
+	public Powerups(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public Powerups() {
+		this.name = null;
+		this.price = 0;
+	}
+
+	public int getQuantity() {
+		return (quantity);
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public abstract int multiplier();
 }
